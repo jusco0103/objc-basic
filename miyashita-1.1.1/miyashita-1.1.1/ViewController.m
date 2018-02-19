83c41c9926e000115c6fa284ebacb3559e13851c
jusco0103 <rysk.myst.biz@gmail.om>
2018-02-20 04:43:03 +0900

    [add] 課題1.1.1実装完了
    ViewController.m 22~32行目に実装内容を記載

--- miyashita-1.1.1/miyashita-1.1.1/ViewController.m
+++ miyashita-1.1.1/miyashita-1.1.1/ViewController.m
@@ -14,11 +14,23 @@
 
 @implementation ViewController
 
-
-zaaaaa
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//1.1.1 BOOL型、NSString型、NSInteger型、NSNumber型の変数を定義し、コンソールにログを出力する。(NSLog)
+
+    //変数宣言
+    BOOL logA=YES;
+    NSString *logB=@"Test";
+    NSInteger logC=10;
+    NSNumber *logD=@100;
+
+    //NSLog表示
+    NSLog(@"BOOL=%d",logA);
+    NSLog(@"NSString=%@",logB);
+    NSLog(@"NSInteger=%ld",(long)logC);
+    NSLog(@"NSNumber=%@",logD);
+
 }
 
 
