17ed08dc4cedec1fa8d4c2c03604cc025aadffbc
jusco0103 <rysk.myst.biz@gmail.om>
2018-02-21 19:01:43 +0900

    [add]2.1.1実装完了
    Localized.String(Base)の#9にラベルの表示文言記載、UIViewController.mの#20~24にラベル内容記載

---
+++ miyashita-2.1.1/miyashita-2.1.1/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  miyashita-2.1.1
+//
+//  Created by 宮下良介 on 2018/02/21.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UILabel *label = [[UILabel alloc] init];
+    label.text = NSLocalizedString(@"スタート", nil);
+    [label sizeToFit];
+    label.center = self.view.center;
+    [self.view addSubview:label];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
