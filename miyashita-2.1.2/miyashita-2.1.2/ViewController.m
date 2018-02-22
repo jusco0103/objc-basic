1fa8a5d12f152e09fcabfb1061c381c1bd3650b2
jusco0103 <rysk.myst.biz@gmail.om>
2018-02-22 17:19:16 +0900

    [add]2.1.2作成中

---
+++ miyashita-2.1.2/miyashita-2.1.2/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  miyashita-2.1.2
+//
+//  Created by 宮下良介 on 2018/02/22.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+UIImage* image = [UIImage imageNamed:@"Image"];
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
