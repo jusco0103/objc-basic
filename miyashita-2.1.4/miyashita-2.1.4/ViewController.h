637759be5daa01b15193601f13996762fe60b208
jusco0103 <rysk.myst.biz@gmail.om>
2018-02-27 16:14:24 +0900

    [add]2.1.4実装完了
    ViewConroller.hの#12に実装
    ViewConroller.mの#19〜39に実装
    Main.StoryboardにUIButtonを実装

---
+++ miyashita-2.1.4/miyashita-2.1.4/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  miyashita-2.1.4
+//
+//  Created by 宮下良介 on 2018/02/27.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *ButtonPush;
+
+
+@end
+
