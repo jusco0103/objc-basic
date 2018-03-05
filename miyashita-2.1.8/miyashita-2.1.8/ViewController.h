963f647830aa4a828d409f88ff22985ed3638a77
jusco0103 <rysk.myst.biz@gmail.om>
2018-03-05 20:08:41 +0900

    [add]2.1.8 作成

---
+++ miyashita-2.1.8/miyashita-2.1.8/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  miyashita-2.1.8
+//
+//  Created by 宮下良介 on 2018/03/02.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
---
+++ miyashita-2.1.8/miyashita-2.1.8/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  miyashita-2.1.8
+//
+//  Created by 宮下良介 on 2018/03/02.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController()
+
+@property (weak, nonatomic) IBOutlet UILabel *testlbl;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
