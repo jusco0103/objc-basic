05f4ae86455dff7a920037a79d9c36ce9b395d9d
jusco0103 <rysk.myst.biz@gmail.om>
2018-03-09 09:12:19 +0900

    [add]2.1.10作成開始

---
+++ miyashita-2.1.10/miyashita-2.1.10/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  miyashita-2.1.10
+//
+//  Created by 宮下良介 on 2018/03/08.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ miyashita-2.1.10/miyashita-2.1.10/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  miyashita-2.1.10
+//
+//  Created by 宮下良介 on 2018/03/08.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
