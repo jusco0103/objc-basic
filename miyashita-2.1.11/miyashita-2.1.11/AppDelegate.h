10041a59c2a128df3b974cebb061edac05ac9c28
jusco0103 <rysk.myst.biz@gmail.om>
2018-03-12 20:45:27 +0900

    [add]2.1.11 作成中
    機能実装は完了、UIの配置を今後修正。
    PropertyList.plist作成
    CustomTableViewCell.h作成
    CustomTableViewCell.m作成
    ViewController.mにplistの取り込み、TableViewのメソッドを記載

---
+++ miyashita-2.1.11/miyashita-2.1.11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  miyashita-2.1.11
+//
+//  Created by 宮下良介 on 2018/03/12.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ miyashita-2.1.11/miyashita-2.1.11/CustomTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.h
+//  miyashita-2.1.11
+//
+//  Created by 宮下良介 on 2018/03/12.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imagesView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
