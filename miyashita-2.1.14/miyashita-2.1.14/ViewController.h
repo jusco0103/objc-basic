f3dcf6a6da6a66f6b561ad68e99909598dc5ddda
jusco0103 <rysk.myst.biz@gmail.om>
2018-03-14 09:52:22 +0900

    [edit]2.1.14修正
    StoryboardにNavigationController追加

---
+++ miyashita-2.1.14/miyashita-2.1.14/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  miyashita-2.1.14
+//
+//  Created by 宮下良介 on 2018/03/13.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ miyashita-2.1.14/miyashita-2.1.14/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  miyashita-2.1.14
+//
+//  Created by 宮下良介 on 2018/03/13.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
