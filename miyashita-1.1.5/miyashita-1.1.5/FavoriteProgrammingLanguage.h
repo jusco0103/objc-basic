94a9674471a8191aa49d057b9d8ddc2cd9ab2b8c
jusco0103 <rysk.myst.biz@gmail.om>
2018-03-29 21:18:02 +0900

    [edit]1−1−5実装完了

--- miyashita-1.1.5/miyashita-1.1.5/FavoriteProgrammingLanguage.h
+++ miyashita-1.1.5/miyashita-1.1.5/FavoriteProgrammingLanguage.h
@@ -2,15 +2,29 @@
 //  FavoriteProgrammingLanguage.h
 //  miyashita-1.1.5
 //
-//  Created by 宮下良介 on 2018/02/21.
+//  Created by 宮下良介 on 2018/03/29.
 //  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
 //
 
+//
 #import <Foundation/Foundation.h>
-#import "FavoriteProgrammingLanguageDelegate.h"
 
-@interface FavoriteProgrammingLanguage : NSObject <FavoriteProgrammingLanguageDelegate>
+//デリゲートするプロトコルを定義する
+@protocol FavoriteProgrammingLanguageDelegate<NSObject>
 
--(void)joinToInturn;
+//オプショナルなメソッドを記載
+@optional
+- (void)canDoObjC;
+
+@end
+
+//FavoriteProgrammingLanguageクラスを定義
+@interface FavoriteProgrammingLanguage : NSObject
+
+//プロパティでデリゲートを定義する
+@property (weak,nonatomic) id<FavoriteProgrammingLanguageDelegate> delegate;
+
+//メソッドを宣言
+- (void)joinInternShip;
 
 @end
