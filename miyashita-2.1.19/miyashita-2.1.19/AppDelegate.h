18a89016aceaec948fcdb1b273778419002bb3bc
jusco0103 <rysk.myst.biz@gmail.om>
2018-04-15 15:40:50 +0900

    [add]2.1.19実装完了

---
+++ miyashita-2.1.19/miyashita-2.1.19/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  miyashita-2.1.19
+//
+//  Created by 宮下良介 on 2018/04/12.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ miyashita-2.1.19/miyashita-2.1.19/FirstViewController.h
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.h
+//  miyashita-2.1.19
+//
+//  Created by 宮下良介 on 2018/04/12.
+//  Copyright © 2018年 Ryosuke_Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SecondViewController.h"
+
+@interface FirstViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *FVCtextField;
+@end
